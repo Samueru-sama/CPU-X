113f5472d6774c3f2e3f022f120dc1a0af07e2b6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-07 02:28:37 -0400

    Update build_appimage.sh

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -37,18 +37,20 @@ echo "LDAI_UPDATE_INFORMATION=$LDAI_UPDATE_INFORMATION"
 # Bundle deps
 runCmd cp --verbose "$APPDIR/usr/share/applications/io.github.thetumultuousunicornofdarkness.cpu-x.desktop" "$APPDIR"
 runCmd cp --verbose "$APPDIR/usr/share/icons/hicolor/256x256/apps/io.github.thetumultuousunicornofdarkness.cpu-x.png" "$APPDIR"
-runCmd mv --verbose "$APPDIR"/usr/share "$APPDIR"
 runCmd mv --verbose "$APPDIR"/usr "$APPDIR"/shared
 
 runCmd wget "${WGET_ARGS[@]}" "https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 runCmd chmod --verbose a+x ./lib4bin
 runCmd ./lib4bin -p -v -s -k \
 	--dst-dir "$APPDIR" \
-	"$APPDIR"/shared/bin/* \
+	"$APPDIR"/shared/bin/cpu-x \
 	/usr/lib/"$ARCH"-linux-gnu/libvulkan*.so* \
 	/usr/lib/"$ARCH"-linux-gnu/libgirepository-*.so* \
 	/usr/lib/"$ARCH"-linux-gnu/gtk-*/*/immodules/*.so \
 	/usr/lib/"$ARCH"-linux-gnu/gdk-pixbuf-*/*/loaders/*
+runCmd ./lib4bin -s --with-wrappe --dst-dir "$APPDIR"/bin "$APPDIR"/shared/bin/cpu-x-daemon
+
+runCmd mv --verbose "$APPDIR"/shared/glib-*/schemas/* "$APPDIR"/share/glib-*/schemas/*
 runCmd ln ./sharun ./AppRun
 runCmd ./sharun -g
 
